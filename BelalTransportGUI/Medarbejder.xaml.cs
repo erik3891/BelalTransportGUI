6c8bf8d401e94b2e198673516d9d8b92b2386c99
Erik Willert Hansen <erik3891@edu.eal.dk>
2019-05-06 10:51:33 +0200

    Master

--- BelalTransportGUI/Medarbejder.xaml.cs
+++ BelalTransportGUI/Medarbejder.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BelalTransportGUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Medarbejder.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Medarbejder : Window
     {
-        public MainWindow()
+        public Medarbejder()
         {
             InitializeComponent();
         }
-
     }
 }
