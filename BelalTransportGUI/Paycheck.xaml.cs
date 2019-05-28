b7542d5ee1f7578bd77a352ae93eae2fe66cdcba
Erik Willert Hansen <erik3891@edu.eal.dk>
2019-05-28 14:39:59 +0200

    lønseddel virker i GUI

--- BelalTransportGUI/Paycheck.xaml.cs
+++ BelalTransportGUI/Paycheck.xaml.cs
@@ -37,11 +37,27 @@ namespace BelalTransportGUI
             }
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Paycalc_Click(object sender, RoutedEventArgs e)
         {
+            double number1 = Int32.Parse(Norpaytime.Text);
+            double number2 = Int32.Parse(Norpay.Text);
+            double number3 = Int32.Parse(Overpaytime.Text);
+            double number4 = Int32.Parse(Overpay.Text);
+            double.TryParse(deductionlabel.Content.ToString(),out double Deduction);
 
+            double Pay = (number1 * number2) + (number3 * number4);
+            Paylabel.Content = Pay;
+            double ATP;
+            double.TryParse(ATPlabel.Content.ToString(), out ATP);
+            double AM = ((Pay - ATP)-(Deduction / 12))* 0.08;
+            AMlabal.Content = AM.ToString("#.##");
         }
 
-        
+        private void BacktoEmployeeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            EmployeeWindow employeeWindow = new EmployeeWindow();
+            employeeWindow.Show();
+            this.Close();
+        }
     }
 }
