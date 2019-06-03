b76a3af561caf041a4bd2bcd65e27d9e5461dc2c
ahmad <ahmad_fayez2004@hotmail.com>
2019-06-03 03:56:18 +0100

    navn

--- BelalTransportGUI/Application/Controller.cs
+++ BelalTransportGUI/Application/Controller.cs
@@ -10,13 +10,7 @@ namespace BelalTransportGUI
     {
         public void GetUdgifter()
         {
-            Udgifter udgifter = new Udgifter();
-            Resultat resultat = new Resultat();
-            
-            resultat.udgifterSum.Text = udgifter.den.Text.ToString();
-            
-            resultat.Show();
-            udgifter.Close();
+        
 
             
         }
